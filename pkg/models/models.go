589fed682a070dc8f85dcd91bcdff0f233489adf
Vincent <vincent.lebrun@leroymerlin.fr>
2023-11-29 16:47:58 +0100

    change some struct

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -3,28 +3,31 @@ package models
 import "time"
 
 type Personne struct {
-	ID      int    `json:"id"`
-	Name    string `json:"name"`
-	Surname string `json:"surname"`
-	Age     string `json:"age"`
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Surname  string `json:"surname"`
+	Age      string `json:"age"`
+	Phone    string `json:"phone"`
+	Mail     string `json:"mail"`
+	Password string `json:"password"`
 }
 
 type Event struct {
 	IdEvent int       `json:"id_event"`
 	Name    string    `json:"name"`
 	Date    time.Time `json:"date"`
-	Town    Town      `json:"town"`
+	Town    []Town    `json:"town"`
 }
 
 type Town struct {
-	IDVille int    `json:"id_town"`
-	Name    string `json:"name"`
+	IdTown int    `json:"id_town"`
+	Name   string `json:"name"`
 }
 
 type Trajet struct {
-	IDTrajet    int    `json:"id_trajet"`
-	Conducteur  string `json:"conducteur"`
-	Depart      Town   `json:"depart"`
-	Destination Town   `json:"destination"`
-	Horaire     string `json:"horaire"`
+	IdTravel    int       `json:"id_travel"`
+	Driver      string    `json:"driver"`
+	Start       Town      `json:"start"`
+	Destination Town      `json:"destination"`
+	Hourly      time.Time `json:"hourly"`
 }
